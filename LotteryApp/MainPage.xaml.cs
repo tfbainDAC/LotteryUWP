4080712ad9ceee7b0029cf3a09b1076b5350927c
Tracey Bain <t.bain@dundeeandangus.ac.uk>
2019-12-05 17:06:43 +0000

    Changes to design to add gradients

--- LotteryApp/MainPage.xaml.cs
+++ LotteryApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LotteryApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,12 +7,14 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,59 +29,162 @@ namespace LotteryApp
         public MainPage()
         {
             this.InitializeComponent();
-            this.Loaded += MainPage_Loaded;
-            this.mainFrame.Navigated += MainFrame_Navigated;
-
-            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
         }
-        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            if (mainFrame.CanGoBack)
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        // List of ValueTuple holding the Navigation Tag and the relative Navigation Page
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            //("home", typeof(HomePage)),
+            ("cust", typeof(Customer)),
+            ("lotto", typeof(Lotto)),
+            ("euro", typeof(Euro)),
+        };
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // You can also add items in code.
+            NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem
             {
-                e.Handled = true;
-                mainFrame.GoBack();
+                Content = "My content",
+                Icon = new SymbolIcon((Symbol)0xF1AD),
+                Tag = "content"
+            });
+            // _pages.Add(("content", typeof(MyContentPage)));   THIS WOULD ADD A NEW PAGE IN CODE
+
+            // Add handler for ContentFrame navigation.
+            ContentFrame.Navigated += On_Navigated;
+
+            // NavView doesn't load any page by default, so load home page.
+            NavView.SelectedItem = NavView.MenuItems[0];
+            // If navigation occurs on SelectionChanged, this isn't needed.
+            // Because we use ItemInvoked to navigate, we need to call Navigate
+            // here to load the home page.
+            NavView_Navigate("home", new EntranceNavigationTransitionInfo());
+
+            // Add keyboard accelerators for backwards navigation.
+            var goBack = new KeyboardAccelerator { Key = VirtualKey.GoBack };
+            goBack.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(goBack);
+
+            // ALT routes here
+            var altLeft = new KeyboardAccelerator
+            {
+                Key = VirtualKey.Left,
+                Modifiers = VirtualKeyModifiers.Menu
+            };
+            altLeft.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(altLeft);
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                         NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
             }
         }
 
-        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        // NavView_SelectionChanged is not used in this example, but is shown for completeness.
+        // You will typically handle either ItemInvoked or SelectionChanged to perform navigation,
+        // but not both.
+        private void NavView_SelectionChanged(NavigationView sender,
+                                              NavigationViewSelectionChangedEventArgs args)
         {
-            btnBack.IsEnabled = mainFrame.CanGoBack;
-            btnForward.IsEnabled = mainFrame.CanGoForward;
+            if (args.IsSettingsSelected == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                var navItemTag = args.SelectedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
         }
 
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
         {
-            mainFrame.Navigate(typeof(Pages.Euro));
+            Type _page = null;
+            //if (navItemTag == "settings")
+            //{
+            //    _page = typeof(SettingsPage);
+            //}
+            //else
+            {
+                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+                _page = item.Page;
+            }
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                ContentFrame.Navigate(_page, null, transitionInfo);
+            }
         }
 
-        private void OnGoBackClick(object sender, RoutedEventArgs e)
+        private void NavView_BackRequested(NavigationView sender,
+                                           NavigationViewBackRequestedEventArgs args)
         {
-            if (mainFrame.CanGoBack) mainFrame.GoBack();
+            On_BackRequested();
         }
 
-        private void OnGoForwardClick(object sender, RoutedEventArgs e)
+        private void BackInvoked(KeyboardAccelerator sender,
+                                 KeyboardAcceleratorInvokedEventArgs args)
         {
-            if (mainFrame.CanGoForward) mainFrame.GoForward();
+            On_BackRequested();
+            args.Handled = true;
         }
 
-        private void OpenSplitViewClick(object sender, RoutedEventArgs e)
+        private bool On_BackRequested()
         {
-            this.mainSplitView.IsPaneOpen = !this.mainSplitView.IsPaneOpen;
+            if (!ContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            ContentFrame.GoBack();
+            return true;
         }
 
-        private void OnGoEuroClick(object sender, RoutedEventArgs e)
+        private void On_Navigated(object sender, NavigationEventArgs e)
         {
-            mainFrame.Navigate(typeof(Pages.Euro));
-        }
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
 
-        private void OnGoCustomerClick(object sender, RoutedEventArgs e)
-        {
-            mainFrame.Navigate(typeof(Pages.Customer));
-        }
+            //if (ContentFrame.SourcePageType == typeof(SettingsPage))
+            //{
+                // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
+            //    NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+            //    NavView.Header = "Settings";
+            //}
+            //else 
+            if (ContentFrame.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
 
-        private void OnGoLottoClick(object sender, RoutedEventArgs e)
-        {
-            mainFrame.Navigate(typeof(Pages.Lotto));
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                NavView.Header =
+                    ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+            }
         }
     }
 }
