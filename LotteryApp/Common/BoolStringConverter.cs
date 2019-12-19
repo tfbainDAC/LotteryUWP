381493cb52553023b94ef8da8edc8993b70ca965
Tracey Bain <t.bain@dundeeandangus.ac.uk>
2019-12-19 16:08:46 +0000

    Added validation example within row 3 of content.xaml

---
+++ LotteryApp/Common/BoolStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+
+namespace LotteryApp.Common
+{
+    internal class BoolStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return bool.Parse(value.ToString());
+        }
+    }
+}
