0d9477e3ad4e16dd830aa2a547bb0bb87b73bba8
Tracey Bain <t.bain@dundeeandangus.ac.uk>
2020-02-21 15:50:22 +0000

    added tests

---
+++ UnitTestTickets/CustomerTests.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tickets;
+
+namespace UnitTestTickets
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void TestCustomerToString()
+        {
+            //arrange
+            String Name = "Young";
+            String Phone = "09876765";
+            String Email = "Young.Hat @Dun.ac.uk";
+            Customer testCust = new Customer(Name, Phone, Email);
+            string expected = String.Format("Name: {0} \nPhone Number: {1} \n" +
+                                             "Email: {1} \n", Name, Phone, Email);
+            //act
+            string response = testCust.ToString();
+
+            //assert
+            Assert.AreEqual(response, expected);
+
+        }
+    }
+}
